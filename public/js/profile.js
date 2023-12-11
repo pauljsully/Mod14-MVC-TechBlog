0d6b711265480d2731959d241b985bae26103aec
Paul Sullivan <paul8sullivan@gmail.com>
2023-12-11 03:11:32 -0500

    added js logic

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,46 +1,47 @@
 const newFormHandler = async (event) => {
-  event.preventDefault();
+	event.preventDefault();
 
-  const name = document.querySelector('#blog-title').value.trim();
-  const description = document.querySelector('#blog-desc').value.trim();
+	const title = document.querySelector('#blog-title').value.trim();
 
-  if (name && description) {
-    const response = await fetch(`/api/blogs`, {
-      method: 'POST',
-      body: JSON.stringify({ name, description }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+	const description = document.querySelector('#blog-desc').value.trim();
 
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert('Failed to create blog');
-    }
-  }
+	if (title && description) {
+		const response = await fetch(`/api/blogs`, {
+			method: 'POST',
+			body: JSON.stringify({ title, description }),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+
+		if (response.ok) {
+			document.location.replace('/dashboard');
+		} else {
+			alert('Failed to create blog');
+		}
+	}
 };
 
 const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
+	if (event.target.hasAttribute('data-id')) {
+		const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/blogs/${id}`, {
-      method: 'DELETE',
-    });
+		const response = await fetch(`/api/blogs/${id}`, {
+			method: 'DELETE',
+		});
 
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert('Failed to delete blog');
-    }
-  }
+		if (response.ok) {
+			document.location.replace('/dashboard');
+		} else {
+			alert('Failed to delete blog');
+		}
+	}
 };
 
 document
-  .querySelector('.new-blog-form')
-  .addEventListener('submit', newFormHandler);
+	.querySelector('.new-blog-form')
+	.addEventListener('submit', newFormHandler);
 
 document
-  .querySelector('.blog-list')
-  .addEventListener('click', delButtonHandler);
+	.querySelector('.blog-list')
+	.addEventListener('click', delButtonHandler);
